0d7cbf6a0383af29a349890c3def1b7f6f7688c6
Jacob Marsland <jm4rsland@gmail.com>
2024-09-16 17:58:17 +0100

    mid iteration 2 - 16/09/2024

--- buttons.py
+++ buttons.py
@@ -1,1 +1,23 @@
-# Code for buttons to be written here and then imported.
+import pygame
+
+class Buttons:
+    instances = []
+
+    def __init__(self, button_name, x, y):
+        self.name = button_name
+        self.x_pos = x
+        self.y_pos = y
+        Buttons.instances.append(self)
+
+    def get_name(self):
+        return self.name
+    
+    def get_x_pos(self):
+        return self.x_pos
+    
+    def get_y_pos(self):
+        return self.y_pos
+    
+    def set_function_():
+        pass
+    
