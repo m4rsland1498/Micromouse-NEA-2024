27f143881b9c13fcbcce09636363c7c053acc778
Jacob Marsland <jm4rsland@gmail.com>
2024-09-17 19:05:28 +0100

    iteration 2 - 17/09 19.05

--- buttons.py
+++ buttons.py
@@ -17,19 +17,15 @@ class Buttons:
     
     def get_y_pos(self):
         return self.y_pos
-    
-    def set_function_(self):
-        pass
-
 
     def draw(self, mouse, pygame, window, button_font, clicked):
         button_text = button_font.render(self.get_name(), True, (255,255,255))
 
-        if self.x_pos <= mouse[0] <= self.x_pos+140 and self.y_pos <= mouse[1] <= self.y_pos+40:
-            pygame.draw.rect(window, (150,150,150), [self.x_pos, self.y_pos, 140, 40])
+        if self.get_x_pos() <= mouse[0] <= self.get_x_pos()+140 and self.get_y_pos() <= mouse[1] <= self.get_y_pos()+40:
+            pygame.draw.rect(window, (150,150,150), [self.get_x_pos(), self.get_y_pos(), 140, 40])
         else:
-            pygame.draw.rect(window, (0,0,0), [self.x_pos, self.y_pos, 140, 40])
+            pygame.draw.rect(window, (0,0,0), [self.get_x_pos(), self.get_y_pos(), 140, 40])
 
-        window.blit(button_text, ((self.x_pos)+5, (self.y_pos)+10))
+        window.blit(button_text, (self.get_x_pos()+5, self.get_y_pos()+10))
 
     
