0b120d67e67ce6c3f7a9638a91efb195fd8688a2
Jacob Marsland <jm4rsland@gmail.com>
2024-09-20 19:56:13 +0100

    iteration 3 - 20/09/2024

--- mouse.py
+++ mouse.py
@@ -6,5 +6,5 @@ class mouse_sprite(object):
         self.width_height = 20
 
     def draw(self, window, pygame):
-        mouse_image = pygame.draw.rect(window, (0, 0, 255), [self.x, self.y, self.width_height, self.width_height])
-        #window.blit(mouse_image)
+        mouse_image = pygame.draw.rect(window, (0, 0, 255),
+        [self.x, self.y, self.width_height, self.width_height])
