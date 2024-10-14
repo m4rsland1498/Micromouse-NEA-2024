231b91f4b4d1bd661e8b83c51d57498fc3072047
m4rsland1498 <jm4rsland@gmail.com>
2024-10-14 11:01:52 +0100

    dfs start

--- dfs.py
+++ dfs.py
@@ -1,29 +1,33 @@
-# when dfs is first called, visited = [] and discovered_maze = []
+undicovered_maze = [['X' for _ in range(22)] for _ in range(22)]
 
 def dfs(maze, x, y, visited, discovered_maze):
 
     visited.append([x,y])
 
     directions = [
-                  [1,0] # right
-                  [0,1] # down
-                  [-1,0] # left
-                  [0,-1] # up
+                  [1,0], # right
+                  [0,1], # down
+                  [-1,0], # left
+                  [0,-1], # up
                   ]
     
-    if maze[x][y] == "G":
-        return discovered_maze
-    else:
-        if x != 20 and maze[x+1][y] != 0 and [x+1, y] not in visited:
-            pass
+    neighbours = []
+    for i in directions:
+        neighbours.append([x+i[0], y+i[1]])
 
-        elif y != 20 and maze[x][y+1] != 0 and [x, y+1] not in visited:
-            pass
+    for i in neighbours:
+        if maze[i[0]][i[1]] == 0:
+            discovered_maze[i[0]][i[1]] = 0
+        
+        elif i not in visited and i[0] != 0 and i[0] != 21 and i[1] != 0 and i[1] != 21:
+            discovered_maze[i[0]][i[1]] = 1
+            dfs(maze, i[0], i[1], visited, discovered_maze)
 
-        elif x != 1 and maze[x-1][y] != 0 and [x-1, y] not in visited:
-            pass
+    return visited, discovered_maze
 
-        elif y!= 1 and maze[x][y-1] != 0 and [x, y-1 not in visited]:
-            pass
+from mazes import maze1
+
+search = dfs(maze1, 1, 1, [], undicovered_maze)
+print(search[1])
 
         
