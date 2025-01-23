72bfc5c28c94195a992dae51281599cc410c90a4
m4rsland1498 <jm4rsland@gmail.com>
2025-01-23 11:11:43 +0000

    begin creation of graphs, need labels, need colours, need second graph (bar chart of efficiencies)

--- mmStatistics.py
+++ mmStatistics.py
@@ -1,4 +1,29 @@
 import matplotlib.pyplot as plt
+import numpy as np
 
-plt.plot([1,2,3,4])
-plt.show()
+def statsWindow():
+    with open("userMice.txt", "r") as f:
+        lines = f.readlines()
+    miceWRD = [] # mice With Run Data
+    for line in lines:
+        name, values = line.split(":")
+        try:
+            speed, acceleration, minutes, seconds, length = map(str, values.split(","))
+            miceWRD.append([name, speed, acceleration, minutes, seconds, length])
+        except:
+            pass
+    
+    plt.style.use("_mpl-gallery")
+
+    # make data and plot
+    fig, ax = plt.subplots()
+    for i in miceWRD:
+        time = int(i[3])*60+int(i[4]) # time in seconds
+        length = int(i[5])
+
+        ax.plot([0, time], [length,length], label=i[0],linewidth=2.0)
+
+    plt.show()
+
+
+statsWindow() # for testing purposes only
